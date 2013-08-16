56d8b8322b2fe16ea4b3d7b5833d9043f4de3865
dluetke <dluetke@gmail.com>
2013-08-16 16:45:59 +0200

    Updated filesystem structure

--- routes/homeview.js
+++
@@ -1,8 +0,0 @@
-
-/*
- * GET home page.
- */
-
-exports.homeview = function(req, res){
-  res.render('homeview', { title: 'Recordspace.com - your record store!' });
-};
--- rsapp.js
+++ rsapp.js
@@ -1,3 +1,8 @@
+/* 
+ * Recordspaces main applications file
+ * 
+ * DLH - 16.08.2013
+ */
 
 /**
  * Module dependencies.
@@ -5,7 +10,7 @@
 
 var express = require('express')
   , backbone = require('backbone')
-  , routes = require('./routes/homeview')
+  , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
@@ -14,14 +19,16 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/public/views');
+app.set('views', __dirname + '/pub/views');
 app.set('view engine', 'jade');
 app.set('title', 'Recordspaces.com - organize your docs!');
+
+// setting application behaviour
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'pub')));
 app.use(app.router);
 
 // development only
@@ -29,9 +36,10 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.homeview);
+app.get('/', routes.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
